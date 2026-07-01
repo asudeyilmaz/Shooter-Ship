1bf3e99ad43cd3ab96ea5a3ced6fec6f3624da10
Asude <asudeyilmaz76@gmail.com>
2026-07-01 18:19:38 +0300

    proje oluşturuldu ve satir sonları crlf yerine lf olarak güncellendi

---
+++ Shooter Ship/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody physic;
+    [SerializeField] int speed;
+
+    public Boundary boundary;
+
+    void Start()
+    {
+        physic = GetComponent<Rigidbody>(); // gemide physic adlı rigidbody oldugunu anlaması için yazdık
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        physic.velocity = movement * speed;
+
+        Vector3 position = new Vector3(
+            Mathf.Clamp(physic.position.x, boundary.xMin, boundary.xMax),
+            0,
+            Mathf.Clamp(physic.position.z, boundary.zMin, boundary.zMax)
+            );
+
+        physic.position = position;
+
+    }
+}
